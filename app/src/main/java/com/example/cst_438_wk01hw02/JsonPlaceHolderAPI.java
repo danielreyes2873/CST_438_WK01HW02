3fd1d9af369519747036d99224c996a57dda700c
Daniel <danielreyes2873@gmail.com>
2021-09-01 21:48:15 -0700

    Initial Commit

---
+++ app/src/main/java/com/example/cst_438_wk01hw02/JsonPlaceHolderAPI.java
@@ -0,0 +1,11 @@
+package com.example.cst_438_wk01hw02;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface JsonPlaceHolderAPI {
+    @GET("posts")
+    Call<List<Post>> getPosts();
+}
